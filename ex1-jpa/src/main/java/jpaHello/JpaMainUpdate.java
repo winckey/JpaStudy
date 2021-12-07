0f0eebd6565d6d23189a84d3d7a2e4631927b0a2
JangHyunJin <winckey3@gmail.com>
2021-12-07 18:22:26 +0900

    1207 학습  : jpa 사용법

---
+++ ex1-jpa/src/main/java/jpaHello/JpaMainUpdate.java
@@ -0,0 +1,40 @@
+package jpaHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMainUpdate {
+
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 팩토리는 딱 하나만 하나만 만들어야 한다. 어플리케이션 로딩시점에
+
+        EntityManager entityManager = emf.createEntityManager();
+        // 트렌잭션 단위로 manager를 사용한다.
+        EntityTransaction et = entityManager.getTransaction();
+        // jpa 는 반듯이 트렌잭션 단위로 움직여야함
+        et.begin();
+
+        //정석코드 이거를 꼭 따르자 (스프링에서는 알아서 다해줌)
+        try {
+            Member findMember = entityManager.find(Member.class , 1L);
+            System.out.println("id = " +findMember.getId());
+            findMember.setName("test2");
+            //자바 객체에서 값만 바꿔도 db값이 바뀐다.
+            //jpa에서 객체를 가져오면 jpa가 지속적으로 객체를 관리하고 커밋직전에 변경사항들을 쿼리를 날리고
+            //아래 et.commit이 실행된다.
+            et.commit();
+        } catch (Exception e) {
+            et.rollback();
+        } finally {
+            entityManager.close();
+        }
+        /////////////
+
+        emf.close();
+    }
+}
